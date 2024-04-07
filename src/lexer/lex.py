78c72d03fbd2cf928c70f3604ec0715bd334524d
fc63327 <fc63327@alunos.ciencias.ulisboa.pt>
2024-04-07 21:51:36 +0200

    initial lex.py file

--- src/lexer/lex.py
+++ src/lexer/lex.py
@@ -1,1 +1,51 @@
+# coding=utf-8
+
+# flake8: noqa ANN001
+
+# List of token names
+tokens = (
+    "COMMA",
+    "VAR"
+)
+
+reservedKeywords = (
+    "var"
+)
+
+# Regular expression rules for simple tokens
+t_COMMA = r","
+
+
+
 #TODO implement
+
+
+
+# Build the lexer
+from ply import lex as lex
+import sys
+
+lexer = lex.lex()
+
+if __name__ == "__main__":
+
+    if len(sys.argv) > 1:
+        f = open(sys.argv[1], "r")
+        data = f.read()
+        f.close()
+    else:
+        data = ""
+        '''while True:
+            try:
+                data += raw_input() + "\n"
+            except:
+                break'''
+
+    lex.input(data)
+
+    # Tokenize
+    while True:
+        tok = lex.token()
+        if not tok:
+            break  # No more input
+        print(tok)
